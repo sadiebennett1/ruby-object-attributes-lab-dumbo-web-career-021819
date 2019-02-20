ab5dfcb1471aca30052076eeaee5f4cac40d805d
sadiebennett1 <sadiebenn1@gmail.com>
2019-02-20 08:43:24 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+
+
+class Person
+    def job
+        @job
+    end
+
+    def job=(persons_job)
+        @job = persons_job
+    end
+
+    def name
+        @name
+    end
+
+    def name=(persons_name)
+        @name = persons_name
+
+    end
+end
